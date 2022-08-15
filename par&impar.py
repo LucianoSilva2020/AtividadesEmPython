fb8b16cd4e930333de7700efbcc916aeeb4ccbad
Aluno da Infinity School <100245367+RayanLopes@users.noreply.github.com>
2022-08-15 20:08:40 -0300

    ..

---
+++ par&impar.py
@@ -0,0 +1,16 @@
+
+import os
+os.system('cls')
+lista = []
+lista2 = []
+verde = '\033[1;36;42m'
+for var in range(1, 6):
+    numero = int(input(f'{verde} Digite o {var}º numero:'))
+    if numero % 2 == 0:
+        lista.append(numero)
+    else:
+        lista2.append(numero)    
+
+
+print(f'Os números pares são {lista}.')
+print(f'E os numeros impares são {lista2}.')    
