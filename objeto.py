8165c8a10fcb6c7044c061360569577031075c1d
Aluno da Infinity School <100245367+RayanLopes@users.noreply.github.com>
2022-08-15 21:10:38 -0300

    ..

--- objeto.py
+++ objeto.py
@@ -1,4 +1,5 @@
 import os
+from traceback import print_tb
 os.system('cls')
 nomeEnota = {}
 tamanho = int(input('Quantos alunos deseja resgistrar?'))
@@ -6,5 +7,5 @@ for var in range(1, tamanho + 1):
     nome = input(f'Digite o nome do {var}° aluno:')
     nota = float(input('Digite a nota:'))
     nomeEnota[nome] = nota
-
-print(nomeEnota)
+ver = input('Deseja visualizar a note de quem ?')
+print(f'Você solicitou a nota de {ver} que foi {nomeEnota[ver]}.')
